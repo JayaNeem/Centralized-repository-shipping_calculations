c9ad34e1217ac5882c118c818a384695d09388a1
JayaNeem <jaya.neem@gmail.com>
2025-12-24 00:21:14 -0500

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by JayaN
+ # Here is a new update by JayaNeem
+# Here is another update by JayaN
