38b24ab6cea44b124356eadb261efb8e061718dd
JayaNeem <jaya.neem@gmail.com>
2025-12-24 00:21:25 -0500

    Revert "added additional line to Shipping_Cost_Calculator.py"
    This reverts commit f893f9d327df40c568608b786ede8509cc9fa6b4.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a new update by JayaNeem
-# Here is another update by JayaN
+ # Here is a new update by JayaN
